941d1ee49130b007b935e7a44989405890440db3
shuvo18103107 <18103107@iubat.edu>
2020-10-13 01:12:01 +0600

    Necessary things Updated and ready for hosting

---
+++ js/common.js
@@ -0,0 +1,12 @@
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+   
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-200px";
+    }
+    prevScrollpos = currentScrollPos;
+}
